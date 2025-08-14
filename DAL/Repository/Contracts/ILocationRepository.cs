b692338a53a1f0c542c1eb56916e5c331ed32bd7
Brandyn Britton <brandynbb96@gmail.com>
2025-08-14 10:50:32 +0800

    update(location): Mapped out initial location repository requirements

--- DAL/Repository/Contracts/ILocationRepository.cs
+++ DAL/Repository/Contracts/ILocationRepository.cs
@@ -1,7 +1,30 @@
+using DAL.Models;
+
 namespace DAL.Repository.Contracts;
 
-public class ILocationRepository
+public interface ILocationRepository
 {
+    /// <summary>
+    /// Finds the location by its city name
+    /// </summary>
+    /// <param name="cityName"></param>
+    /// <returns>Models.Location</returns>
+    Task<ICollection<Location>> FindLocationByCityAsync(string cityName);
     
+    /// <summary>
+    /// Find Location by its geolocation (latitude and longitude)
+    /// This is useful for finding locations that are close to a given point.
+    /// </summary>
+    /// <param name="latitude"></param>
+    /// <param name="longitude"></param>
+    /// <returns>Models.Location</returns>
+    Task<ICollection<Location>> FindLocationByGeolocationAsync(double latitude, double longitude);
+    
+    /// <summary>
+    /// Finds the location by its unique identifier
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Models.Location</returns>
+    Task<Location> FindLocationByIdAsync(int id);
     
 }
