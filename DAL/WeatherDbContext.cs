f4708155e382ea067f81a2b9b9320fad8280812b
Brandyn Britton <brandynbb96@gmail.com>
2025-08-14 16:54:46 +0800

    fix(timestamps): Fix auto-adding timestamps

--- DAL/WeatherDbContext.cs
+++ DAL/WeatherDbContext.cs
@@ -16,6 +16,7 @@ public class WeatherDbContext : DbContext
     }
 
     public DbSet<Location> Locations { get; set; }
+    public DbSet<GeocodeSearch> GeocodeSearches { get; set; }
 
     // Configure EF to create a Sqlite database at the specified path, locally
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -27,6 +28,34 @@ public class WeatherDbContext : DbContext
             .UseSqlite("Data Source=../DAL/Database/WeatherApp.db;Cache=Shared;")
             .UseSeeding(SeedData)
             .UseAsyncSeeding(SeedDataAsync);
+    }
+
+    public override int SaveChanges()
+    {
+        var entries = ChangeTracker.Entries()
+            .Where(e => e.Entity is TimeStampedEntity &&
+                        (e.State == EntityState.Added || e.State == EntityState.Modified));
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added) ((TimeStampedEntity)entry.Entity).CreatedAt = DateTime.UtcNow;
+            ((TimeStampedEntity)entry.Entity).UpdatedAt = DateTime.UtcNow;
+        }
+
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries()
+            .Where(e => e.Entity is TimeStampedEntity &&
+                        (e.State == EntityState.Added || e.State == EntityState.Modified));
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added) ((TimeStampedEntity)entry.Entity).CreatedAt = DateTime.UtcNow;
+            ((TimeStampedEntity)entry.Entity).UpdatedAt = DateTime.UtcNow;
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 
     private static async Task SeedDataAsync(DbContext context, bool isDevelopment,
