7119dcfc8d9cb6622983d7301ec05f32074addd0
Brandyn Britton <brandynbb96@gmail.com>
2025-08-14 14:16:45 +0800

    update(SearchLocationByCityDTO): Added required to `CityName`

--- DAL/Dtos/Location/SearchLocationByCityDto.cs
+++ DAL/Dtos/Location/SearchLocationByCityDto.cs
@@ -7,15 +7,15 @@ public class SearchLocationByCityDto : IModelToDto<Models.Location, SearchLocati
     [Required]
     [MaxLength(200)]
     [MinLength(2)]
-    public string CityName { get; set; }
-    
+    public required string CityName { get; set; }
+
     public static SearchLocationByCityDto MapToDto(Models.Location model)
     {
         // Validate the model before mapping
         // Rely on model field annotations for validation
         if (model == null) throw new ArgumentNullException(nameof(model), "Model cannot be null");
 
-        return new SearchLocationByCityDto()
+        return new SearchLocationByCityDto
         {
             CityName = model.City
         };
