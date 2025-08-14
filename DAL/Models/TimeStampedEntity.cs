ba06a4b358e2c83deec46f5bc421374bf6643d00
Brandyn Britton <brandynbb96@gmail.com>
2025-08-14 14:17:19 +0800

    fix(TimeStampedEntity): Added proper generated values

--- DAL/Models/TimeStampedEntity.cs
+++ DAL/Models/TimeStampedEntity.cs
@@ -1,10 +1,12 @@
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAL.Models;
 
 public abstract class TimeStampedEntity
 {
-    [Timestamp] public DateTime CreatedAt { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedAt { get; set; }
 
-    [Timestamp] [ConcurrencyCheck] public DateTime UpdatedAt { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime UpdatedAt { get; set; }
 }
