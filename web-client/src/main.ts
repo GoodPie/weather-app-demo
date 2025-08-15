977e0dede7b9c9f7b1fb0495957566a11a63e594
Brandyn Britton <brandynbb96@gmail.com>
2025-08-15 09:58:57 +0800

    update(frontend): Added PrimeVue Volt - Good mix between new library and just tailwind

--- web-client/src/main.ts
+++ web-client/src/main.ts
@@ -2,11 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App);
+app.use(PrimeVue, {
+  unstyled: true
+});
 
 app.use(createPinia())
 app.use(router)
--- web-client/vite.config.ts
+++ web-client/vite.config.ts
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,8 +20,8 @@ export default defineConfig({
     }
   },
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "./src") }
+    ]
   },
 })
